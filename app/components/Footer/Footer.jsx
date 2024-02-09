866edab2fef2f3c9efd6fcc7fc17f4e312df9b9e
bkleinb78 <y2bk728@aol.com>
2024-02-08 22:30:00 -0600

    created the nextjs project + added components folder

---
+++ app/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer>
+        <p>© {new Date().getFullYear()} Wolfman's Entertainment and Karaoke</p>{" "}
+      </footer>
+    );
+  };
+  
+  export default Footer;
